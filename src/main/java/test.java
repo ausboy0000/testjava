941ebe18c77f8a24b4fbe09a4d3d61404f700ff1
ausboy0000 <64524511+ausboy0000@users.noreply.github.com>
2021-07-17 12:01:34 +1000

    test

---
+++ src/main/java/test.java
@@ -0,0 +1,8 @@
+import java.text.SimpleDateFormat;
+import java.util.logging.SimpleFormatter;
+
+public class test {
+    public static void main(String[] args) {
+        new SimpleFormatter();
+    }
+}
