86bba7a3883bd810cf01568913b04ae17504421a
ausboy0000 <64524511+ausboy0000@users.noreply.github.com>
2021-07-17 12:07:33 +1000

    test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,5 +4,7 @@ import java.util.logging.SimpleFormatter;
 public class test {
     public static void main(String[] args) {
         new SimpleFormatter();
+        System.out.println();
     }
+
 }
